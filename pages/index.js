8701f0765005cc8a4c8ad58bb1edc10fc62e8da9
Tryjapa <thiago.yamaguchi@gmail.com>
2020-11-11 01:06:17 +0000

    sidebar

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,22 @@
+import Router from "next/dist/next-server/lib/router/router";
+import { Route, Switch } from "react-router-dom";
+
+const { default: navbar } = require("./components/navbar");
+
+import Navbar from "./components/navbar"
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom" 
+
 function Home(){
-    return <div>Home</div>
+    return (
+    <>
+    <Router>
+    <Navbar></Navbar>
+    <Switch>
+        <Route path="/"></Route>
+    </Switch>
+    </Router>
+    
+    </>)
 }
 
 export default Home
