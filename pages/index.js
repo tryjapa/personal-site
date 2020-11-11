4e143f0722888aad50c13d06e47d36a687421a4b
Tryjapa <thiago.yamaguchi@gmail.com>
2020-11-11 01:09:48 +0000

    sidebar fix

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import { Route, Switch } from "react-router-dom";
 const { default: navbar } = require("./components/navbar");
 
 import Navbar from "./components/navbar"
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom" 
+
 
 function Home(){
     return (
