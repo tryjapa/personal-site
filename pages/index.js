8c2368b8e0742f8930b845d8beca2ad85a1bd515
Tryjapa <thiago.yamaguchi@gmail.com>
2020-11-14 00:54:15 +0000

    update files

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,7 @@
-import Router from "next/dist/next-server/lib/router/router";
-import { Route, Switch } from "react-router-dom";
-
-const { default: navbar } = require("./components/navbar");
-
-import Navbar from "./components/navbar"
-
-
 function Home(){
     return (
     <>
-    <Router>
-    <Navbar></Navbar>
-    <Switch>
-        <Route path="/"></Route>
-    </Switch>
-    </Router>
-    
+    Home    
     </>)
 }
 
